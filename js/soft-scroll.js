63c7ed0accd035c73aabe2f7ddbb3e3f20449884
Mike Thomas <mikepthomas@outlook.com>
2017-05-17 01:08:05 +0100

    implement soft scroll to page top

---
+++ js/soft-scroll.js
@@ -0,0 +1,2 @@
+define(["require","exports","jquery"],function(e,t,o){Object.defineProperty(t,"__esModule",{value:!0}),t.default=function(e){var t=e.attr("href");e.on("click",function(e){e.preventDefault(),o("html, body").animate({scrollTop:o(t).offset().top},"slow")})}});
+//# sourceMappingURL=soft-scroll.js.map
